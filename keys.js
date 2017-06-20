8a9fa51d469b349061ef54fb7f400bf36ea6fc52
Joshua Serrano-Patron <jrsp@Joshuas-MacBook-Pro.local>
2017-06-20 13:39:02 -0400

    working

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'dpp7XpI1iLXFPt9GfkbKd2Weg',
+  consumer_secret: 'XfiW5haDjeLlr4FR6Sw6IjiJbNcXDYppMk22S4CS2btWNqZPQB',
+  access_token_key: '595045903-WOg802cAUTpLvnFgCFOo7JEFdEW9J14hmVO4pge7',
+  access_token_secret: 'jzzvZbZjVsT1VDDauN6lhI0r7Wf3Xs4pE4K50gpJ0WZ8p',
+}
